02c6fcba083f583403a9220a78d2d0a9f2c6021b
Tuan Pham <tuanpham37@knights.ucf.com>
2016-11-27 01:00:56 -0500

    added timer, url change listener, associative array of sites

--- background.js
+++ background.js
@@ -1,12 +1,33 @@
-chrome.tabs.onActivated.addListener(function() {
-  printHost();
-});
+var sites =  {};
+var currentSite;
 
-var printHost = function () {
+//listeners
+chrome.tabs.onActivated.addListener(function() {
+  updateCurrentSite();
+});
+//add older chrome support in future, also won't work unless you add the check for onhashchange for some reason
+//inconsistent, doesn't always work, replace with setinterval
+if ("onhashchange" in window) {
+  window.onhashchange = function() {
+    updateCurrentSite();
+  };
+}
+
+setInterval(timer, 1000);
+function timer() {
+    sites[currentSite]++;
+    console.log(currentSite + " " + sites[currentSite]);
+}
+
+var updateCurrentSite = function () {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     var parser = document.createElement('a');
     parser.href = url;
-    console.log(parser.host);
+    if (!(parser.host in sites)) {
+      sites[parser.host] = 1;
+      currentSite = parser.host;
+    }
+    currentSite = parser.host;
   });
 }
