8294cde3375ccab7397a109055e6548367fcc977
tunaphish <tuanpham37@knights.ucf.edu>
2016-11-28 01:24:18 -0500

    removed listeners and just checked website during setInterval.

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 var sites =  {};
 var currentSite;
 
+//removed listeners and just checked the website when incrementing timer
+/*
 //listeners
 chrome.tabs.onActivated.addListener(function() {
   updateCurrentSite();
@@ -12,9 +14,12 @@ if ("onhashchange" in window) {
     updateCurrentSite();
   };
 }
+*/
 
+//fix bug where it messes up while looking at inspection page. 
 setInterval(timer, 1000);
 function timer() {
+    updateCurrentSite();
     sites[currentSite]++;
     console.log(currentSite + " " + sites[currentSite]);
 }
@@ -24,10 +29,8 @@ var updateCurrentSite = function () {
     var url = tabs[0].url;
     var parser = document.createElement('a');
     parser.href = url;
-    if (!(parser.host in sites)) {
-      sites[parser.host] = 1;
-      currentSite = parser.host;
-    }
+    if (!(parser.host in sites))
+       sites[parser.host] = 1;
     currentSite = parser.host;
   });
 }
