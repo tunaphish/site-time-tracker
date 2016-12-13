f2b4d0087b668decf603c3353d2021954c3aa0c2
tunaphish <tuanpham37@knights.ucf.edu>
2016-12-12 23:35:07 -0500

    formatted time

--- background.js
+++ background.js
@@ -1,31 +1,10 @@
-
-//removed listeners and just checked the website when incrementing timer
-/*
-//listeners
-chrome.tabs.onActivated.addListener(function() {
-  updateCurrentSite();
-});
-//add older chrome support in future, also won't work unless you add the check for onhashchange for some reason
-//inconsistent, doesn't always work, replace with setinterval
-if ("onhashchange" in window) {
-  window.onhashchange = function() {
-    updateCurrentSite();
-  };
-}
-*/
-
 var currentSite;
 
-(function (){
-
-})(window);
-
-//BUG when inspecting background page it will throw erros
 //set interval runs every second which acts as the timer
 setInterval(timer, 1000);
 function timer() {
     updateCurrentSite();
-    //update site timer
+    //update daily, weekly, and lifetime-timer
     localStorage.setItem(currentSite,Number(localStorage.getItem(currentSite)) + 1);
     updateTimer(localStorage.getItem(currentSite));
 }
@@ -50,10 +29,6 @@ var updateTimer = function(time) {
   var views = chrome.extension.getViews({
     type: "popup"
   });
-  for (var i = 0; i < views.length; i++) {
-    views[i].document.getElementById('site-title').innerHTML = currentSite;
-    views[i].document.getElementById('daily-timer').innerHTML = "Lifetime: " + hours + ":" + minutes + ":" + seconds;
-    views[i].document.getElementById('weekly-timer').innerHTML = "Lifetime: " + hours + ":" + minutes + ":" + seconds;
-    views[i].document.getElementById('lifetime-timer').innerHTML = "Lifetime: " + hours + ":" + minutes + ":" + seconds;
-  }
+  views[0].document.getElementById('site-title').innerHTML = currentSite;
+  views[0].document.getElementById('lifetime-timer').innerHTML = "Lifetime: " + new Date(time * 1000).toISOString().substr(11, 8);
 }
