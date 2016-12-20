2d6ce9ba6bce24e9fb74cec40be20356cc248e61
Tuan Pham <tuanpham37@knights.ucf.com>
2016-12-20 17:43:50 -0500

    added sites list section

--- popup.js
+++ popup.js
@@ -4,9 +4,20 @@ var port = chrome.extension.connect({
 port.onMessage.addListener(function(msg) {
      updatePopup(msg)
 });
-
 function updatePopup(timer) {
   document.getElementById('site-title').innerHTML = timer.site;
-  document.getElementById('session-timer').innerHTML = "Session: " + new Date(timer.sessionTime * 1000).toISOString().substr(11, 8);
-  document.getElementById('lifetime-timer').innerHTML = "Lifetime: " + new Date(timer.localTime * 1000).toISOString().substr(11, 8);
+  document.getElementById('session-timer').innerHTML = "Session: " + formatTime(timer.sessionTime);
+  document.getElementById('lifetime-timer').innerHTML = "Lifetime: " + formatTime(timer.localTime);
 }
+function formatTime(seconds) {
+  return new Date(seconds * 1000).toISOString().substr(11, 8);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var div = document.getElementById('sites-list')
+  for (var i = 0; i < localStorage.length; i++){
+      var p = document.createElement('P');
+      p.innerHTML = localStorage.key(i) + " " + formatTime(localStorage.getItem(localStorage.key(i)));
+      div.appendChild(p);
+  }
+});
